469b809887c619bc860ea03987a2b01ede04ae23
aituming <wlyuan1115@gmail.com>
2017-10-29 16:39:13 +0800

    draw circle

---
+++ DrawCircle/src/com/example/drawcircle/CircleRenderer.java
@@ -0,0 +1,43 @@
+package com.example.drawcircle;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView.Renderer;
+
+import com.example.drawcircle.utils.Logit;
+
+public class CircleRenderer implements Renderer{
+	private static final String TAG = "CircleRenderer";
+	private Circle triangle;
+	private Context  context;
+	public CircleRenderer(Context context) {
+		// TODO Auto-generated constructor stub
+		this.context = context;
+	}
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+		Logit.d(TAG, "onSurfaceCreated.");
+		triangle = new Circle(context);
+		GLES30.glClearColor(1, 1, 1, 1);
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+		Logit.d(TAG, "onSurfaceChanded...");
+		GLES30.glViewport(0, 0, width, height);
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO Auto-generated method stub
+		Logit.d(TAG, "onDrawFrame");
+		GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+		triangle.draw();
+	}
+
+}
